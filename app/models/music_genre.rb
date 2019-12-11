569427a87601de32ae3b0712dd85bc0ae5a296f8
diegoarvz4 <darvz.gtz@gmail.com>
2019-12-11 10:12:31 -0600

    Updated music_genre.rb and musical_instrument.rb

--- app/models/music_genre.rb
+++ app/models/music_genre.rb
@@ -1,4 +1,5 @@
 class MusicGenre < ApplicationRecord
   has_and_belongs_to_many :music_teachers
   validates_presence_of :category
+  validates :category, uniqueness: true
 end
--- app/models/musical_instrument.rb
+++ app/models/musical_instrument.rb
@@ -3,4 +3,5 @@
 class MusicalInstrument < ApplicationRecord
   has_and_belongs_to_many :music_teachers
   validates_presence_of :kind
+  validates :kind, uniqueness: true
 end
