ab77ac572418c844b72dafe4f24321fb8961426a
diegoarvz4 <darvz.gtz@gmail.com>
2019-12-12 13:23:15 -0600

    Secrete configuration key updated

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -11,7 +11,7 @@ class JsonWebToken
 
   def self.decode(token)
     # get payload; first index in decoded Array
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body = JWT.decode(token, HMAC_SECRET.to_s)[0]
     HashWithIndifferentAccess.new body
     # rescue from all decode errors
   rescue JWT::DecodeError => e
