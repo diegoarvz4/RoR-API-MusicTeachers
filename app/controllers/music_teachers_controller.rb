b43d7df8b28b949f1467cb78732f06ceb5c65954
diegoarvz4 <darvz.gtz@gmail.com>
2019-12-21 15:47:36 -0600

    Deleted Columns From Associated Tables in MusciTeachers

--- app/controllers/music_teachers_controller.rb
+++ app/controllers/music_teachers_controller.rb
@@ -4,7 +4,17 @@ class MusicTeachersController < ApplicationController
   def index
     @music_teachers = MusicTeacher.all
     render json: @music_teachers,
-           include: %i[appointments music_genres musical_instruments],
+           include: {
+             appointments: {
+               except: %i[created_at updated_at]
+             },
+             music_genres: {
+               except: %i[created_at updated_at]
+             },
+             musical_instruments: {
+               except: %i[created_at updated_at]
+             }
+           },
            except: %i[created_at updated_at],
            status: :ok
   end
